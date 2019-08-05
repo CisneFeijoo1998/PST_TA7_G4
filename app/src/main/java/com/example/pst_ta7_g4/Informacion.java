b1d6d4b1cbc6c52fb3cbfe3c0aaa67681ffff9f9
CisneFeijoo1998 <mdfeijoo@espol.edu.ec>
2019-08-05 00:35:58 -0500

    Creacion de nueva Activity llamada Mapa, donde se muestra las ubicaciones de los restaurante en el mapa

--- app/src/main/java/com/example/pst_ta7_g4/Informacion.java
+++ app/src/main/java/com/example/pst_ta7_g4/Informacion.java
@@ -2,9 +2,12 @@ package com.example.pst_ta7_g4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
 
 public class Informacion extends AppCompatActivity {
     Spinner horario;   //Instanciamos el objeto Spinner
@@ -20,4 +23,15 @@ public class Informacion extends AppCompatActivity {
         //Se setea el spinner con el adapter
         horario.setAdapter(adapter);
     }
+
+    public void volver(View view) {
+        Intent i = new Intent(this,MainActivity.class );
+        startActivity(i);
+    }
+
+    public void mapa(View view) {
+        Intent i = new Intent(this, Mapa.class );
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/example/pst_ta7_g4/MainActivity.java
+++ app/src/main/java/com/example/pst_ta7_g4/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.pst_ta7_g4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void button(View view) {
+        Intent i = new Intent(this, Informacion.class );
+        startActivity(i);
+    }
+
 }
