0cc1a4f87d08293dc1dcc2c89a6be1f0272c08d3
Hugo <hugoandericau@bitbucket.org>
2019-08-06 19:11:32 -0500

    comentarios agregados

--- app/src/main/java/com/example/pst_ta7_g4/MainActivity.java
+++ app/src/main/java/com/example/pst_ta7_g4/MainActivity.java
@@ -8,29 +8,27 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
 
-
+    //Metodo asignado a img_Encabezado que permite acceder al Acicity "Infromacion"
     public void toInformacion(View view) {
         Intent i = new Intent(this, Informacion.class );
         startActivity(i);
     }
 
-
+    //Metodo asignado a img_reserva que permite acceder al Acicity "reservaActivity"
     public void toReserva(View view) {
         Intent i = new Intent(this,reservaActivity.class);
         startActivity(i);
     }
 
+    //Metodo asignado a img_menu que permite acceder al Acicity "Menu"
     public void toMenu(View view) {
         Intent i = new Intent(this, Menu.class);
         startActivity(i);
